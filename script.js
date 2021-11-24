6a00454ac341d071c31d6462e1cc544d6e0d987e
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-11-24 12:33:43 +0100

    Aggiunta effetto neon css più javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+// var i = 0;
+// var txt = 'Hello World'; /* The text */
+// var speed = 150; /* The speed/duration of the effect in milliseconds */
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("text").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// 
